b6999c4ee7654df788cebc109dbfa31eac479f3c
unknown <sajedislam019@gmail.com>
2024-10-30 22:08:48 +0600

    file add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
+  /*Preloader */
+  
+  window.addEventListener("load", function () {
+    document.body.classList.add("loaded");
+  });
+
+  /*Preloader */
+ 
 /* Banner slider*/
   $(document).ready(function(){
     $(".banner-slider").owlCarousel({
@@ -11,3 +19,42 @@
       autoplayHoverPause: true // Pause on hover
     });
   });
+
+  // toggleBar menu 
+  const mobile_nav = document.querySelector(".togle-bar-btn");
+
+  const nav_header = document.querySelector(".header-area")
+
+  const toggleNavbar = () => {
+    nav_header.classList.toggle("active")
+  }
+
+  mobile_nav.addEventListener("click", () => toggleNavbar());
+
+  // Optional: Add solid background on scroll stacky header
+  window.addEventListener('scroll', () => {
+      const header = document.querySelector('.header-area');
+      header.classList.toggle('scrolled', window.scrollY > 50);
+  });
+
+
+
+
+/*scroll bar*/
+ $(window).on('scroll', function () {
+   if ($(this).scrollTop() > 100) {
+     $('#scrollToTop').fadeIn();
+   } else {
+     $('#scrollToTop').fadeOut();
+   }
+ });
+
+ // Scroll to top when button is clicked
+ $('#scrollToTop').on('click', function (e) {
+   e.preventDefault();
+   $('html, body').animate({
+     scrollTop: 0
+   }, 'slow');
+ });
+ 
+/*scroll bar*/
